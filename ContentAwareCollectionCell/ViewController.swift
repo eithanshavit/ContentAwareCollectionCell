336a3ef6633bb2c0200988e0a48c3d6553b16a61
Eithan Shavit <eithanshavit@gmail.com>
2015-04-15 23:24:52 -0700

    Initial Commit

---
+++ ContentAwareCollectionCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContentAwareCollectionCell
+//
+//  Created by  Eithan Shavit on 4/15/15.
+//  Copyright (c) 2015  Eithan Shavit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
